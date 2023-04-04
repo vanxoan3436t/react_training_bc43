5d38f6173cca37810a37baaf80c20963a80e1f0c
VanXoan <vansoan20@gmail.com>
2023-04-04 20:25:39 +0700

    init_project

---
+++ src/Components/HomeComponents/Header.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className='bg-dark text-white p-5 fs-1 text-center'>
+        Header component
+      </div>
+    )
+  }
+}
